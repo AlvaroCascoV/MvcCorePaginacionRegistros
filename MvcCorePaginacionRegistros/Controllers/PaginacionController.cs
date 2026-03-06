66aac772307bdfcc5136cd2814901de9557960ce
AlvaroCascoV <alvarocascovalero@gmail.com>
2026-03-06 13:19:07 +0100

    paginacion individual

---
+++ MvcCorePaginacionRegistros/Controllers/PaginacionController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCorePaginacionRegistros.Models;
+using MvcCorePaginacionRegistros.Repositories;
+
+namespace MvcCorePaginacionRegistros.Controllers
+{
+    public class PaginacionController : Controller
+    {
+        private RepositoryHospital repo;
+
+        public PaginacionController(RepositoryHospital repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> RegistroVistaDepartamento(int? posicion)
+        {
+            if (posicion == null)
+            {
+                posicion = 1;
+            }
+            int numRegistros = await this.repo.GetNumeroRegistrosVistaDepartamentosAsync();
+            //PRIMERO = 1
+            //ULTIMO = 4
+            //ANTERIOR = posicion - 1
+            //SIGUIENTE = posicion + 1
+            int siguiente = posicion.Value + 1;
+            if(siguiente > numRegistros)
+            {
+                //si queremos dar la vuelta a la paginacion, el siguiente del ultimo seria el 1
+                siguiente = numRegistros;
+            }
+            int anterior = posicion.Value - 1;
+            if(anterior < 1)
+            {
+                //si queremos dar la vuelta a la paginacion, el anterior del primero seria el numRegistros
+                anterior = 1;
+            }
+            ViewData["ULTIMO"] = numRegistros;
+            ViewData["SIGUIENTE"] = siguiente;
+            ViewData["ANTERIOR"] = anterior;
+            VistaDepartamento departamento = await this.repo.GetVistaDepartamentoAsync(posicion.Value);
+            return View(departamento);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MvcCorePaginacionRegistros/Repositories/RepositoryHospital.cs
+++ MvcCorePaginacionRegistros/Repositories/RepositoryHospital.cs
@@ -1,7 +1,18 @@
-﻿using MvcCorePaginacionRegistros.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCorePaginacionRegistros.Data;
+using MvcCorePaginacionRegistros.Models;
 
 namespace MvcCorePaginacionRegistros.Repositories
 {
+    #region SQl
+//    alter view V_DEPARTAMENTOS_INDIVIDUAL
+//as
+//select CAST(
+//    ROW_NUMBER() over (order by DEPT_NO) as int) 
+//		as POSICION,
+//		DEPT_NO, DNOMBRE, LOC from DEPT
+//go
+    #endregion
     public class RepositoryHospital
     {
         private HospitalContext context;
@@ -9,6 +20,17 @@ namespace MvcCorePaginacionRegistros.Repositories
         public RepositoryHospital(HospitalContext context)
         {
             this.context = context;
+        }
+
+        public async Task<int> GetNumeroRegistrosVistaDepartamentosAsync()
+        {
+            return await context.VistaDepartamentos.CountAsync();
+        }
+
+        public async Task<VistaDepartamento> GetVistaDepartamentoAsync(int posicion)
+        {
+            VistaDepartamento departamento = await this.context.VistaDepartamentos.Where(z => z.Posicion == posicion).FirstOrDefaultAsync();
+            return departamento;
         }
     }
 }
